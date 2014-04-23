5e7d579701846a1057728133fb1dd1b9feea69be
proyectoalerta <alerta.proyecto@gmail.com>
2014-04-24 00:30:28 +0200

    RF3
    Requisito funcional 3
    Lectura de la temperatura desde un dispositivo móvil.

---
+++ Código Fuente/alerta/CorreoDestino.h
@@ -0,0 +1,7 @@
+// your Gmail username, formatted as a complete email address, eg "bob.smith@gmail.com"
+const String GMAIL_USER_NAME = "alerta.proyecto@gmail.com";
+// your Gmail password
+const String GMAIL_PASSWORD = "jesussoto";
+
+// the email address you want to send the email to, eg "jane.doe@temboo.com"
+const String TO_EMAIL_ADDRESS = "alerta.proyecto@gmail.com";
